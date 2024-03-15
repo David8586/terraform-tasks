6698e8ca90ccae25fcce92da012aa44cafb93b4e
David8586 <147060632+David8586@users.noreply.github.com>
2024-03-15 13:00:08 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "vamsi" {
   ami                    = "ami-022661f8a4a1b91cf"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   availability_zone      = "ap-southeast-1"
   key_name               = "terra"
   tags = {
